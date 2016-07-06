289fe776d9b81bdf1085c4c0aab4fdd0b4fef738
unknown <m.diaz.fernandez@MW7H53WQC48734.dir.svc.accenture.com>
2016-07-06 16:25:16 +0200

    Now, it does random forest. Pending interpretations.

--- Machine_learning_script_v2.R
+++ Machine_learning_script_v2.R
@@ -11,3 +11,27 @@ train_set <- read.csv("training.csv", stringsAsFactors = FALSE)
 
 download.file(path_test, "test.csv")
 test_set <- read.csv("test.csv", stringsAsFactors = FALSE)
+
+missing_data <- data.frame(names = colnames(train_set), NAS = 0)
+
+for (i in 1:length(missing_data$names)){
+  y <- sum(is.na(train_set[,i]))
+  z <- ifelse(is.na(sum(train_set[,i]=='')),0,sum(train_set[,i]=='') )
+  missing_data$NAS[i] <- z + y
+}
+
+delete <- which(missing_data$NAS>10000) #REmove the variables that lack data
+train_set2 <- train_set[,-delete]
+train_set2 <- train_set2[,-(1:6)]
+test_set2 <- test_set[,-delete]
+test_set2 <- test_set2[,-(1:6)]       
+
+Correlated <- caret::findCorrelation(cor(train_set2[,-54]), cutoff=0.9) #Correlated variables.
+
+if (length(Correlated)>0) {
+  tc <- trainControl(method = "cv", number = 7, verboseIter=FALSE , preProcOptions="pca", allowParallel=TRUE)
+} else {
+  tc <- trainControl(method = "cv", number = 7, verboseIter=FALSE , allowParallel=TRUE)
+}
+
+rf <- train(classe ~ ., data = train_set2, method = "rf", trControl= tc) # Random forest
